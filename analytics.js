656f5b273c9271a10f2cc498bdcc8b856eed61d4
Grunet <sadikshahidain@gmail.com>
2023-03-06 00:39:55 +0000

    no-op the code

--- analytics.js
+++ analytics.js
@@ -10,57 +10,64 @@ function decorateGtagWithAccessibilityInformation() {
     }
 
     // Media Features - code resolves synchronously
-
-    // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion
-    captureMediaFeatureBasedPreference({
+    try {
+      // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion
+      captureMediaFeatureBasedPreference({
         mediaFeature: "prefers-reduced-motion",
         possibleValues: ["no-preference", "reduce"],
-    });
+      });
 
-    // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme
-    captureMediaFeatureBasedPreference({
-        mediaFeature: "prefers-color-scheme",
-        possibleValues: ["light", "dark"],
-    });
+      // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme
+      captureMediaFeatureBasedPreference({
+          mediaFeature: "prefers-color-scheme",
+          possibleValues: ["light", "dark"],
+      });
 
-    // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/inverted-colors - Safari only currently
-    captureMediaFeatureBasedPreference({
-        mediaFeature: "inverted-colors",
-        possibleValues: ["none", "inverted"],
-    });
+      // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/inverted-colors - Safari only currently
+      captureMediaFeatureBasedPreference({
+          mediaFeature: "inverted-colors",
+          possibleValues: ["none", "inverted"],
+      });
 
-    // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/forced-colors
-    captureMediaFeatureBasedPreference({
-        mediaFeature: "forced-colors",
-        possibleValues: ["none", "active"],
-    });
+      // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/forced-colors
+      captureMediaFeatureBasedPreference({
+          mediaFeature: "forced-colors",
+          possibleValues: ["none", "active"],
+      });
+    } catch (error) {
+      console.error(error);
+    }
+    
 
     // Keyboard detection code - code resolves asynchronously
-
     (function captureAndEmitKeyboardData() {
-            const intervalId = setInterval(function checkForKeyboardUsage() {
+            try {
+                  const intervalId = setInterval(function checkForKeyboardUsage() {
       
-            const focusedElement = document.querySelector(":focus-visible");
-        
-            if (!focusedElement) {
-                return;
+                  const focusedElement = document.querySelector(":focus-visible");
+              
+                  if (!focusedElement) {
+                      return;
+                  }
+              
+                  // Ignore common false positives for click/touch users
+                  const tagNameUpperCased = focusedElement.tagName.toUpperCase();
+                  if (tagNameUpperCased === "INPUT" || tagNameUpperCased === "TEXTAREA") {
+                      return;
+                  }
+              
+                  if (focusedElement.contentEditable === "true") {
+                      return;
+                  }
+              
+                  accessibilityEventParameters["uses-keyboard"] = true;
+              
+                  clearInterval(intervalId);
+              }, 500);
+            } catch (error) {
+              console.error(error);
             }
-        
-            // Ignore common false positives for click/touch users
-            const tagNameUpperCased = focusedElement.tagName.toUpperCase();
-            if (tagNameUpperCased === "INPUT" || tagNameUpperCased === "TEXTAREA") {
-                return;
-            }
-        
-            if (focusedElement.contentEditable === "true") {
-                return;
-            }
-        
-            accessibilityEventParameters["uses-keyboard"] = true;
-        
-            clearInterval(intervalId);
-        }, 500);
-      })()
+      })();
 
 
     // Helper functions

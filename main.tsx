86a90e74c25a84669643a54df842e8b9a7034307
Grunet <sadikshahidain@gmail.com>
2023-03-22 22:32:33 -0500

    Update Google Analytics property (#43)

--- main.tsx
+++ main.tsx
@@ -91,7 +91,7 @@ function handler(req) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1TB5S4JW40"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q5R7J81KZZ"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
@@ -102,7 +102,7 @@ function handler(req) {
 
           gtag('js', new Date());
 
-          gtag('config', 'G-1TB5S4JW40');
+          gtag('config', 'G-Q5R7J81KZZ');
         </script>
         <script>
           ${minifiedSnippet}

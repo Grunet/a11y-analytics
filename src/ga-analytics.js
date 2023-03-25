4a5e5d992c32602994ae346218c2d9b255336f6d
Grunet <sadikshahidain@gmail.com>
2023-03-25 15:54:32 +0000

    Refactor to be able to take into account plausible's different function signature

--- src/ga-analytics.js
+++ src/ga-analytics.js
@@ -4,6 +4,17 @@ function decorateGtagWithAccessibilityInformation() {
   decorateCustomEventGlobalWithAccessibilityInformation({
     getGlobal: () => {  return window.gtag; }, 
     setGlobal: (value) => { window.gtag = value; },
+    translateArguments: ({ originalArguments, accessibilityEventParameters}) => { 
+        const translatedArguments = [...originalArguments]; // TODO - replace with structuredClone for a true deep copy once it has better browser support
+
+        if (originalArguments.length === 3) {
+          const originalParameters = originalArguments[2];
+          const newParameters = {...originalParameters, ...accessibilityEventParameters};
+          translatedArguments[2] = newParameters;
+        }
+
+        return translatedArguments;
+    },
     onResolutionCallback: ({ name, data: { resolvedValue }}) => {
       window.gtag('event', 'resolvedAccessibilityData', {
         nameOfData: name,

9e7e739f3e15559d174025f915a86ca820f51aa0
Grunet <sadikshahidain@gmail.com>
2023-03-25 16:12:52 +0000

    factoring out to allow for plausible to be built

--- scripts/build.ts
+++ scripts/build.ts
@@ -5,36 +5,40 @@ const outputDir = "./dist/";
 
 await cleanDirectory(outputDir);
 
-await esbuild.build({
-    entryPoints: ['./src/ga-analytics.js'],
-    outfile: './dist/ga-analytics.min.js',
-    bundle: true,
-    minify: true
-  })
-
-const minifiedCode = await Deno.readTextFile("./dist/ga-analytics.min.js");
-
-console.log(minifiedCode);
-console.log("");
-
-const encoder = new TextEncoder();
-const bytes = encoder.encode(minifiedCode);
-const uint8Array = new Uint8Array(bytes);
-const compressedBytes = gzip(uint8Array);
-
-console.log(`Number of bytes: ${uint8Array.length}`);
-console.log(`Number of bytes after gzip compression: ${compressedBytes.length}`);
-
-// Directly pasting the minified JS into main.tsx doesn't work because it runs into issues with not escaping backticks, single quotes, and double quotes
-
-// This takes the minified JS and adjusts it so that it can be pasted directly into a JS string defined with backticks
-const backticksEscapedVersion = minifiedCode.replaceAll("`", "\\`").replaceAll("${","\\${");
-await Deno.writeTextFile(`${outputDir}ga-escapedForBackticks.txt`, backticksEscapedVersion);
+await buildOneAnalyticsSnippet({prefix: "ga"});
 
 Deno.exit();
 
 
 // Helper functions
+
+async function buildOneAnalyticsSnippet({prefix}: {prefix: string}) {
+    await esbuild.build({
+        entryPoints: [`./src/${prefix}-analytics.js`],
+        outfile: `./dist/${prefix}-analytics.min.js`,
+        bundle: true,
+        minify: true
+      })
+    
+    const minifiedCode = await Deno.readTextFile(`./dist/${prefix}-analytics.min.js`);
+    
+    console.log(minifiedCode);
+    console.log("");
+    
+    const encoder = new TextEncoder();
+    const bytes = encoder.encode(minifiedCode);
+    const uint8Array = new Uint8Array(bytes);
+    const compressedBytes = gzip(uint8Array);
+    
+    console.log(`Number of bytes in ./dist/${prefix}-analytics.min.js: ${uint8Array.length}`);
+    console.log(`Number of bytes in ./dist/${prefix}-analytics.min.js after gzip compression: ${compressedBytes.length}`);
+    
+    // Directly pasting the minified JS into main.tsx doesn't work because it runs into issues with not escaping backticks, single quotes, and double quotes
+    
+    // This takes the minified JS and adjusts it so that it can be pasted directly into a JS string defined with backticks
+    const backticksEscapedVersion = minifiedCode.replaceAll("`", "\\`").replaceAll("${","\\${");
+    await Deno.writeTextFile(`${outputDir}${prefix}-escapedForBackticks.txt`, backticksEscapedVersion);
+}
 
 async function cleanDirectory(path: string) {
     const [,error] = await functionalDenoOpen(path);

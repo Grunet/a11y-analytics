334b400930038f9d6757334aa58a003654b8ab40
Grunet <sadikshahidain@gmail.com>
2023-04-28 14:12:18 +0000

    add event names into the parameters

--- src/ga-analytics.js
+++ src/ga-analytics.js
@@ -1,28 +1,43 @@
-import { decorateCustomEventGlobalWithAccessibilityInformation } from "./analytics.js"
+import { decorateCustomEventGlobalWithAccessibilityInformation } from "./analytics.js";
 
 function decorateGtagWithAccessibilityInformation() {
   decorateCustomEventGlobalWithAccessibilityInformation({
-    getGlobal: () => {  return window.gtag; }, 
-    setGlobal: (value) => { window.gtag = value; },
-    translateArguments: ({ originalArguments, accessibilityEventParameters}) => { 
-        const translatedArguments = [...originalArguments]; // TODO - replace with structuredClone for a true deep copy once it has better browser support
-
-        if (originalArguments.length >= 3) {
-          const originalParameters = originalArguments[2];
-          
-          const translatedParameters = {...originalParameters, ...accessibilityEventParameters};
-
-          translatedArguments[2] = translatedParameters;
-        }
-
-        return translatedArguments;
+    getGlobal: () => {
+      return window.gtag;
     },
-    onResolutionCallback: ({ name, data: { resolvedValue }}) => {
-      window.gtag('event', 'resolvedAccessibilityData', {
-        nameOfData: name,
-        resolvedValue,
+    setGlobal: (value) => {
+      window.gtag = value;
+    },
+    translateArguments: (
+      { originalArguments, accessibilityEventParameters },
+    ) => {
+      const translatedArguments = [...originalArguments]; // TODO - replace with structuredClone for a true deep copy once it has better browser support
+
+      if (originalArguments.length >= 3) {
+        const originalParameters = originalArguments[2];
+
+        const eventName = originalArguments[1];
+        const disambiguatedAccessibilityEventParameters = Object.fromEntries(
+          Object.entries(accessibilityEventParameters).map(([key, value]) => {
+            return [`${key} [${eventName}]`, value];
+          }),
+        );
+
+        const translatedParameters = {
+          ...originalParameters,
+          ...disambiguatedAccessibilityEventParameters,
+        };
+
+        translatedArguments[2] = translatedParameters;
+      }
+
+      return translatedArguments;
+    },
+    onResolutionCallback: ({ name, data: { resolvedValue } }) => {
+      window.gtag("event", "resolvedAccessibilityData", {
+        [`${name} [resolvedAccessibilityData]`]: resolvedValue,
       });
-    }
+    },
   });
 }
 
